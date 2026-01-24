a2fec91a520d1033eac725db49f46ac3e879ed0f
egor <krasikovegor711@gmail.com>
2026-01-24 20:08:21 +0500

    feat: integrate Gemini for news paraphrasing and dependencies for it
    - Added GeminiService with Google AI SDK
    - Implemented getGeminiParaphrase for sending request to Gemini api
    - Added errors handling for the requests with logging
    - Added function promptBody to keep with all the rules
    
    - Implemented logback and slf4j api dependencies due to conflict of old versions
    - Implemented coroutines and genai

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,11 @@ dependencies {
     implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
+    implementation("com.google.genai:google-genai:1.36.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.8.0")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("org.slf4j:slf4j-api:2.0.9")
 }
 
 application {
